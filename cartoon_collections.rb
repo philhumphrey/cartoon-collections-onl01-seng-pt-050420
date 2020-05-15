af543bd4ab741ae6f06e66cec16658bfe46fbac5
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 04:17:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
-def roll_call_dwarves(little_names)# code an argument here
-
-  # Your code here
+def roll_call_dwarves(dwarves)
+  
+  each_with_index 
+  
 end
 
 def summon_captain_planet# code an argument here
