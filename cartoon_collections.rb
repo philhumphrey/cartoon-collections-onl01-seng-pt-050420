6da48140fe4dbc813e6ae7bb86a9b2ae61c9dd19
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 04:54:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def long_planeteer_calls(calls)
     end
 
 def find_the_cheese(cheeses)
-  cheeses.find do 
+  cheeses.find do |cheese|
   cheeses.include 
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  end
 end
